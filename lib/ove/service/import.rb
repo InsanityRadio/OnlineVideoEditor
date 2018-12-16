932c1c27d280bfed71cc0b2e7f256368c84b0aa1
Jamie0 <jamie+git@index.hm>
2018-12-16 16:43:07 +0000

    Add import skeleton

---
+++ lib/ove/service/import.rb
@@ -0,0 +1,28 @@
+require 'securerandom'
+
+module OVE
+	module Service
+		# The main interface to externally access the ingest engine. Outside of this class, ingest
+		#  should not be referenced/used in code. 
+		class Ingest < HTTPService
+			enable :sessions
+
+			get '/:service/import' do |service|
+				start_time = params['start_time'].to_i
+				end_time = params['end_time'].to_i
+
+				# 1. Get a list of HLS chunks between start and end
+				# 2. Generate a unique ID for the category name
+				# 3. Store the HLS chunks persistently under that chunk ID
+				# 4. Return.
+
+				my_sources = OVE::Ingest::SourceProvider.instance.sources
+				source = my_sources.find { |s| s.service == service }
+
+				halt 404 unless service
+
+				source.generate_hls start_time * 1000.0, end_time * 1000.0
+			end
+		end
+	end
+end
